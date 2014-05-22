ee568f75b402e1d24f309ce7ab92de6e221ca003
wus <swunderlin@gmail.com>
2014-05-22 20:14:06 +0200

    first working version

--- cert.py
+++ cert.py
@@ -9,6 +9,17 @@
 #   5 $$trainer$$
 
 import sys, os
+from subprocess import call
+
+is_cgi = False
+#check if this is a cgi request
+if 'REQUEST_METHOD' in os.environ :
+	# Import modules for CGI handling 
+	import cgi, cgitb 
+	cgitb.enable()
+	# Create instance of FieldStorage 
+	#form = cgi.FieldStorage() 
+	is_cgi = True
 
 def usage():
 	print """
@@ -20,15 +31,25 @@ def usage():
 #   5 $$trainer$$
 """
 
+inkscape_bin = "/usr/bin/inkscape"
+arg = ["exe", "name", "title", "date", "location", "trainer"]
+
 def main(argv=None):
-	
-	args = ["exe", "name", "title", "date", "location", "trainer"]
+	#print "Content-type: text/plain\n\n"
+	#print argv
+	#print 1
+	#sys.exit(0)
 	
 	if argv is None:
 		argv = sys.argv
 	
-	if (len(sys.argv) < 6):
-		print("Argument error\n")
+	#print "Content-type: text/plain\n\n"
+	#print "Hello %s" % argv[0]
+	#sys.exit(0)
+	
+	if (len(argv) < 6):
+		print("Argument error %d\n") % len(sys.argv)
+		print argv
 		usage()
 		sys.exit(1)
 	
@@ -43,6 +64,7 @@ def main(argv=None):
 	except IOError as e:
 		print "I/O error({0}): {1}".format(e.errno, e.strerror)
 		fp.close()
+		sys.exit(1)
 	except:
 		print "Unexpected error:", sys.exc_info()[0]
 		fp.close()
@@ -52,7 +74,7 @@ def main(argv=None):
 	fp.close()
 	
 	# subsitute values in svg
-	for (i, keyword) in enumerate(args):
+	for (i, keyword) in enumerate(arg):
 		#sys.stdout.write("[%d] %s: %s\n " % (i, keyword, argv[i]))
 		repl = '$$' + keyword + '$$'
 		#sys.stdout.write("%s %s\n" % (repl, argv[i]))
@@ -61,15 +83,88 @@ def main(argv=None):
 	# write temp svg file
 	pid = os.getpid()
 	
-	# process id for temp file
-	sys.stdout.write("%s\n" % os.getpid())
+	# TODO: figure out absolute path
+	file_base = "./tmp/out-%d" % (pid)
+	file_svg = file_base + ".svg"
+	
+	#sys.stdout.write("%s\n" % file_svg)
 	#print buffer;
+	try:
+		fp = open(file_svg, "wb+")
+	except IOError as e:
+		print "I/O error({0}): {1}".format(e.errno, e.strerror)
+		fp.close()
+		sys.exit(1)
+	except:
+		print "Unexpected error:", sys.exc_info()[0]
+		fp.close()
+		sys.exit(1)
+	
+	fp.write(buffer)
+	fp.close()
 	
 	# convert svg to pdf
+	# /usr/bin/inkscape --export-pdf=FILENAME
+	file_pdf = file_base + ".pdf"
+	call([inkscape_bin, "--export-pdf="+file_pdf, file_svg])
+	
+	statinfo = os.stat(file_pdf)
+	#print "Content-type: text/plain\n\n"
+	#print "aaa"
+	#sys.stdout.write("Content-length: %d\n\n" % statinfo.st_size)
+	#sys.exit(0)
 	
 	# stream pdf result
+	sys.stdout.write("Content-type: application/pdf\n")
+	#print file_pdf# + " - %d\n" % statinfo.st_size
+	sys.stdout.write("Content-length: %d\n" % statinfo.st_size)
+	sys.stdout.write("Content-disposition: inline; filename='CD_certifcate.pdf'\n\n")
+	
+	try:
+		fp = open(file_pdf, "r")
+	except IOError as e:
+		print "I/O error({0}): {1}".format(e.errno, e.strerror)
+		fp.close()
+		sys.exit(1)
+	except:
+		print "Unexpected error:", sys.exc_info()[0]
+		fp.close()
+		sys.exit(1)
+	print fp.read()
+	fp.close()
 	
 	
-
 if __name__ == "__main__":
-	sys.exit(main())
+	a = None
+	if (is_cgi):
+		form = cgi.FieldStorage() 
+		
+		a = ["cgi", "", "", "", "", ""]
+		#a.append(form.getValue(args[1]))
+		
+		#print "Content-type: text/html\n\n"
+		#print args.index("name")
+		for i in form.keys():
+			#print i
+			ix = arg.index(i)
+			#print ix
+			#print i + " %d" % ix
+			#print form[i].value + " <br/>"
+			a[ix] = form[i].value
+			#print i + " " + form[i].value +"\n"
+			
+		#print 1
+		#print a[0]
+		#print a[1]
+		#print a[2]
+		#print a[3]
+		#print a[4]
+		#print a[5]
+		#sys.exit(0)
+		#a.append(form.getValue(arg[1]))
+		#a.append(form.getValue(arg[2]))
+		#a.append(form.getValue(arg[3]))
+		#a.append(form.getValue(arg[4]))
+		#a.append(form.getValue(arg[5]))
+		
+	sys.exit(main(a))
